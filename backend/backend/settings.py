a38a8edfb97ebcc611836b4657d27cad1f866ab2
hiepuet1205 <73263193+hiepuet1205@users.noreply.github.com>
2022-12-17 14:40:30 +0700

    install app

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'oauth2_provider',
+    'rest_framework',
+    'drf_yasg',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
